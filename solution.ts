09ddfb971dea03df382a72d77ddffebbd56ed103
greenFruits <mdabdullah6548ctg@gmail.com>
2025-11-27 22:36:09 +0600

    feat: added problem 02 solution

--- solution.ts
+++ solution.ts
@@ -1,13 +1,18 @@
 type FormatValue = string | number | boolean;
-const formatValue = (value: FormatValue): FormatValue => {
+const formatValue = (value: FormatValue): FormatValue | void => {
   if (typeof value === "string") {
     return value.toUpperCase();
   } else if (typeof value === "number") {
     return value * 10;
+  } else if (typeof value === "boolean") {
+    return value ? false : true;
   }
-  return value ? false : true;
 };
 
-console.log(formatValue("hello world"));
-console.log(formatValue(10));
-console.log(formatValue(false));
+const getLength = (input: string | unknown[]): number | void => {
+  if (typeof input === "string") {
+    return input.length;
+  } else if (Array.isArray(input)) {
+    return input.length;
+  }
+};
